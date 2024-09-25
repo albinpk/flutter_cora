3b85eb3232919d1ec113c9f9abe19431541bb4c6
Albin <56157868+albinpk@users.noreply.github.com>
2024-09-25 19:31:21 +0530

    fix: move `flutter_cora` to `packages` directory

--- packages/flutter_cora/lib/src/cora_state.dart
+++ packages/flutter_cora/lib/src/cora_state.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/widgets.dart';
-import 'package:flutter_cora/src/cora_view.dart';
+import 'cora_view.dart';
 
 abstract class CoraState<T extends CoraView> extends State<T> {
   @override
