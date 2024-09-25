187d17f46d79a5687b06757884d7bca5191e227f
Albin <56157868+albinpk@users.noreply.github.com>
2024-09-25 21:39:44 +0530

    doc: add comments for `CoraView` and `CoraState`

--- packages/flutter_cora/lib/src/cora_state.dart
+++ packages/flutter_cora/lib/src/cora_state.dart
@@ -1,7 +1,15 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
+
 import 'cora_view.dart';
 
+/// A base class for the state of a [CoraView].
+///
+/// This class extends the [State] class so all the lifecycle methods like
+/// [initState], [didChangeDependencies], [didUpdateWidget], [dispose] are
+/// available to override.
 abstract class CoraState<T extends CoraView> extends State<T> {
+  @nonVirtual
   @override
   Widget build(BuildContext context) {
     return widget.build(this);
--- packages/flutter_cora/lib/src/cora_view.dart
+++ packages/flutter_cora/lib/src/cora_view.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/widgets.dart';
 
+/// A base class for Flutter widgets that can be used with [CoraState].
+///
+/// It provides a `build` method that is called when the widget needs to be
+/// rebuilt. This method should return the widget to be displayed.
+///
+/// The `createState` method should return an instance of the [CoraState] class that
+/// will be used to store the widget's state.
+///
+/// See also:
+///
+/// * [CoraState], which is the base class for the state of a [CoraView].
 abstract class CoraView<T extends State> extends StatefulWidget {
+  /// Constructs a [CoraView].
   const CoraView({super.key});
 
   @override
   T createState();
 
+  /// The [build] method is called when the widget needs to be rebuilt.
+  ///
+  /// It should return the widget to be displayed.
+  ///
+  /// The [state] parameter contains the state of the widget. The build context can
+  /// be accessed by the `context` getter like `state.context`.
   Widget build(T state);
 }
