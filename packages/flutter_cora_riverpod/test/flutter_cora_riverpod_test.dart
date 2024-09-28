f71e9ddfe27eeb749d1fdffb149331d2271f8b18
Albin <56157868+albinpk@users.noreply.github.com>
2024-09-28 13:55:45 +0530

    fix: add test for cora consumer

--- packages/flutter_cora_riverpod/test/flutter_cora_riverpod_test.dart
+++ packages/flutter_cora_riverpod/test/flutter_cora_riverpod_test.dart
@@ -1,5 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_cora_riverpod/flutter_cora_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('test...', () {});
+  testWidgets('create cora consumer view and state', (tester) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: _TestView(text: 'cora view'),
+      ),
+    );
+    expect(find.text('cora view'), findsOneWidget);
+  });
 }
+
+class _TestView extends CoraConsumerView<_TestState> {
+  const _TestView({required this.text});
+
+  final String text;
+
+  @override
+  _TestState createState() => _TestState();
+
+  @override
+  Widget build(_TestState state) {
+    return Scaffold(
+      body: Text(text),
+    );
+  }
+}
+
+class _TestState extends CoraConsumerState<_TestView> {}
