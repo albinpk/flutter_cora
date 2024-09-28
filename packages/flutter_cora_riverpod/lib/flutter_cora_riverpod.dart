5af37d3ed1b51632c812781ec60b53e0a188c991
Albin <56157868+albinpk@users.noreply.github.com>
2024-09-28 13:51:23 +0530

    fix: update test

--- packages/flutter_cora_riverpod/lib/flutter_cora_riverpod.dart
+++ packages/flutter_cora_riverpod/lib/flutter_cora_riverpod.dart
@@ -1,7 +1,4 @@
 library flutter_cora_riverpod;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/cora_consumer_state.dart';
+export 'src/cora_consumer_view.dart';
