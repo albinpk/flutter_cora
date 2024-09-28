4799e7af2a653ca5c28c675dcc047a39a0714f30
Albin <56157868+albinpk@users.noreply.github.com>
2024-09-28 13:08:13 +0530

    fix: update test

--- packages/flutter_cora/test/flutter_cora_test.dart
+++ packages/flutter_cora/test/flutter_cora_test.dart
@@ -1,7 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_cora/flutter_cora.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('test...', () {
-    expect(1 + 1, 2);
+  testWidgets('create cora view and state', (tester) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: _TestView(text: 'cora view'),
+      ),
+    );
+    expect(find.text('cora view'), findsOneWidget);
   });
 }
+
+class _TestView extends CoraView<_TestState> {
+  const _TestView({required this.text});
+
+  final String text;
+
+  @override
+  _TestState createState() => _TestState();
+
+  @override
+  Widget build(_TestState state) {
+    return Scaffold(
+      body: Text(text),
+    );
+  }
+}
+
+class _TestState extends CoraState<_TestView> {}
