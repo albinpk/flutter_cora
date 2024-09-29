05f30f18140852ee5d51204f0474fc7e0c098065
Albin <56157868+albinpk@users.noreply.github.com>
2024-09-29 13:34:21 +0530

    build: create preVersion script for melos hook

---
+++ scripts/preVersion.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Script for update package version in README.md (usage section)
+
+for pubspecFile in $(git diff --name-only "packages/*/pubspec.yaml")
+do
+    newVersion=$(grep '^version: [0-9]\+.[0-9]\+.[0-9]\+.*$' $pubspecFile | awk '{print $2}')
+    readmeDir="$(dirname $pubspecFile)/README.md"
+    packageName=$(basename $(dirname $pubspecFile))
+    sed -i "s/  $packageName: ^[0-9]\+.[0-9]\+.[0-9]\+.*$/  $packageName: ^$newVersion/" $readmeDir
+done
