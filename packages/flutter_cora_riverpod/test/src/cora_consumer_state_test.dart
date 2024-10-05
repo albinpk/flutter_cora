8087c0c81ed34e2248267dd8dd33d1e04472eb0a
Albin <56157868+albinpk@users.noreply.github.com>
2024-10-05 17:29:48 +0530

    test: add test for  `read`, `watch` and `listen` methods

---
+++ packages/flutter_cora_riverpod/test/src/cora_consumer_state_test.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_cora_riverpod/flutter_cora_riverpod.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('cora consumer state', () {
+    testWidgets('read method', (tester) async {
+      final provider = Provider((ref) => 7);
+      late int value;
+
+      await tester.pumpWidget(
+        ProviderScope(
+          child: _CallbackWidget(
+            builder: (state) {
+              value = state.read(provider);
+              return const SizedBox();
+            },
+          ),
+        ),
+      );
+
+      expect(value, 7);
+    });
+
+    testWidgets('watch method', (tester) async {
+      final provider = StateProvider((ref) => 1);
+      late CoraConsumerState state;
+      int buildCount = 0;
+
+      await tester.pumpWidget(
+        ProviderScope(
+          child: _CallbackWidget(
+            builder: (s) {
+              state = s;
+              s.watch(provider);
+              buildCount++;
+              return const SizedBox();
+            },
+          ),
+        ),
+      );
+
+      expect(buildCount, 1);
+
+      state.read(provider.notifier).state = 2;
+      await tester.pump();
+
+      expect(buildCount, 2);
+    });
+
+    testWidgets('listen method', (tester) async {
+      final provider = StateProvider((ref) => 1);
+      late CoraConsumerState state;
+      int buildCount = 0;
+      int listenCount = 0;
+
+      await tester.pumpWidget(
+        ProviderScope(
+          child: _CallbackWidget(
+            builder: (s) {
+              state = s;
+              s.listen(
+                provider,
+                (previous, next) => listenCount++,
+              );
+              buildCount++;
+              return const SizedBox();
+            },
+          ),
+        ),
+      );
+
+      expect(buildCount, 1);
+      expect(listenCount, 0);
+
+      state.read(provider.notifier)
+        ..state = 2
+        ..state = 3;
+      await tester.pump();
+
+      expect(buildCount, 1);
+      expect(listenCount, 2);
+    });
+  });
+}
+
+class _CallbackWidget extends CoraConsumerView<_CallbackState> {
+  const _CallbackWidget({required this.builder});
+
+  final Widget Function(CoraConsumerState state) builder;
+
+  @override
+  Widget build(_CallbackState state) => builder(state);
+
+  @override
+  _CallbackState createState() => _CallbackState();
+}
+
+class _CallbackState extends CoraConsumerState<_CallbackWidget> {}
